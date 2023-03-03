92ea7d97435f00d44a94aa15797b69f511aad4f2
Jurgita <jurgita.eicinaite@yahoo.com>
2023-03-03 14:11:39 +0200

    generate list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
 
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        fetch('https://v2.jokeapi.dev/joke/Programming?amount=10')
+            .then(res => res.json())
+            .then(
+                result => {
+                    setIsLoaded(true);
+                    setItems(result);
+                },
+                error => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+    }, []);
+
+    console.log(items);
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <ul>
+                {items.jokes.map(item => {
+                    if (item.type === 'single') {
+                        return <li key={item.id}>{item.joke}</li>;
+                    } else {
+                        return (
+                            <li key={item.id}>
+                                {item.setup} {item.delivery}
+                            </li>
+                        );
+                    }
+                })}
+            </ul>
+        );
+    }
 }
 
 export default App;
